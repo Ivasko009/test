adf36c34bf9239edcc9020666a6647837cac75fb
Ivasko009 <131871287+Ivasko009@users.noreply.github.com>
2024-09-22 15:19:33 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,27 @@
-$wshell = New-Object -ComObject Wscript.Shell
+Add-Type -AssemblyName System.Windows.Forms
 
-$wshell.Popup("Operation Completed",0,"Done",0x1)
+# Funkcia na zobrazenie pop-up správy
+function Show-PopUp {
+    [System.Windows.Forms.MessageBox]::Show("I told you nothing bad will happen", "Attention", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+}
+
+# Sledovanie procesov Windows Defendera
+$processesToMonitor = @("MsMpEng", "MpCmdRun")
+
+# Sledovanie bude prebiehať, kým sa nezistí prvý sken
+$defenderScanDetected = $false
+
+while (-not $defenderScanDetected) {
+    foreach ($process in $processesToMonitor) {
+        $processExists = Get-Process -Name $process -ErrorAction SilentlyContinue
+        if ($processExists) {
+            Show-PopUp
+            $defenderScanDetected = $true
+            break
+        }
+    }
+    Start-Sleep -Seconds 5  # Kontrola každých 5 sekúnd
+}
+
+# Ukončí sledovací skript po prvom skene
+exit
