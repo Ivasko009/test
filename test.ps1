666970b1c8b300b41b3ce1566865402dd76ded39
Ivasko009 <131871287+Ivasko009@users.noreply.github.com>
2024-09-22 14:59:23 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,3 @@
-Add-Type -AssemblyName System.Windows.Forms
+$wshell = New-Object -ComObject Wscript.Shell
 
-# Zobrazenie pop-up správy s upozornením
-[System.Windows.Forms.MessageBox]::Show("Toto je upozornenie!", "Upozornenie", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+$wshell.Popup("Operation Completed",0,"Done",0x1)
